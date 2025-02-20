6bb2c2384e279086417dd779e2bad83d94b31e0e
maslak0v <gmw00d@vk.com>
2025-02-20 23:13:38 +0500

    Add files via upload

---
+++ Jack's_House.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Базовый класс для частей стихотворения
+abstract class Part
+{
+    public List<string> Poem { get; protected set; } = new List<string>();
+
+    public abstract List<string> AddPart(List<string> previousPoem);
+}
+
+// Пример реализации части 1
+class Part1 : Part
+{
+    public override List<string> AddPart(List<string> previousPoem)
+    {
+        var newPoem = new List<string>(previousPoem);
+        newPoem.Add("Вот дом, который построил Джек.");
+        Poem = newPoem;
+        return Poem;
+    }
+}
+
+// Пример реализации части 2
+class Part2 : Part
+{
+    public override List<string> AddPart(List<string> previousPoem)
+    {
+        var newPoem = new List<string>(previousPoem);
+        newPoem.Add("А это пшеница,");
+        newPoem.Add("Которая в темном чулане хранится");
+        newPoem.Add("В доме, который построил Джек.");
+        Poem = newPoem;
+        return Poem;
+    }
+}
+
+// Основная программа
+class Program
+{
+    static void Main(string[] args)
+    {
+        Part1 part1 = new Part1();
+        Part2 part2 = new Part2();
+        // Добавьте остальные части аналогично...
+
+        // Последовательное добавление частей
+        var poem1 = part1.AddPart(new List<string>());
+        var poem2 = part2.AddPart(poem1);
+
+        // Вывод результатов
+        Console.WriteLine("Part1:");
+        foreach (var line in part1.Poem) Console.WriteLine(line);
+
+        Console.WriteLine("\nPart2:");
+        foreach (var line in part2.Poem) Console.WriteLine(line);
+    }
+}
---
+++ Librarian.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+class Program
+{
+    // Коллекция книг с процентом прочтения
+    static ConcurrentDictionary<string, int> books = new ConcurrentDictionary<string, int>();
+
+    static void Main(string[] args)
+    {
+        Thread backgroundThread = new Thread(UpdateReadingProgress);
+        backgroundThread.IsBackground = true;
+        backgroundThread.Start(); // Запускаем фоновый поток
+
+        while (true)
+        {
+            Console.WriteLine("1 - добавить книгу; 2 - вывести список непрочитанного; 3 - выйти");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.WriteLine("Введите название книги:");
+                string title = Console.ReadLine();
+                books.TryAdd(title, 0); // Добавляем книгу с начальным прогрессом 0%
+            }
+            else if (choice == "2")
+            {
+                Console.WriteLine("Список непрочитанных книг:");
+                foreach (var book in books)
+                {
+                    Console.WriteLine($"{book.Key} - {book.Value}%");
+                }
+            }
+            else if (choice == "3")
+            {
+                break; // Выход из программы
+            }
+        }
+    }
+
+    // Фоновый поток для обновления прогресса чтения
+    static void UpdateReadingProgress()
+    {
+        while (true)
+        {
+            Thread.Sleep(1000); // Ждем 1 секунду
+
+            foreach (var book in books.Keys.ToList())
+            {
+                if (books.TryGetValue(book, out int progress))
+                {
+                    if (progress < 100)
+                    {
+                        books[book] = progress + 1; // Увеличиваем прогресс на 1%
+                    }
+                    else
+                    {
+                        books.TryRemove(book, out _); // Удаляем книгу, если прогресс достиг 100%
+                    }
+                }
+            }
+        }
+    }
+}
